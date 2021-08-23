d127e1031a4c1fc1610b2a168c0c9fb98071e14d
Kristina Zobernienė <k.zigelyte@gmail.com>
2021-08-23 08:44:43 +0300

    OneTimeSetUp, OneTimeTearDown, TestCases

--- testing1/namu_darbai_1.cs
+++ testing1/namu_darbai_1.cs
@@ -7,80 +7,48 @@ using OpenQA.Selenium.Support.UI;
 namespace testing1
 {
     public class namu_darbai_1 {
-    [Test]
-        public static void TestTwoInputFields1()
+        
+        private static IWebDriver chrome;
+        
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
         {
-            IWebDriver chrome = new ChromeDriver(); //sukuriamas objektas, jo pavadinimas chrome
+            chrome = new ChromeDriver(); 
+            chrome.Manage().Window.Maximize();
+            chrome.Manage().Timeouts().ImplicitWait =
+                TimeSpan.FromSeconds(10); 
             chrome.Url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
-            //uzdaryti pop-up
             WebDriverWait wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(10));
             wait.Until(d =>
                 d.FindElement(By.CssSelector("#at-cv-lightbox-close"))
                     .Displayed); //d kaip atitikmuo driver, galima irasyti ka nori
             chrome.FindElement(By.CssSelector("#at-cv-lightbox-close")).Click();
-
-            IWebElement input1 = chrome.FindElement(By.Id("sum1"));
-            IWebElement input2 = chrome.FindElement(By.Id("sum2"));
-            IWebElement sumButton = chrome.FindElement(By.CssSelector("#gettotal > button"));
             
-            input1.SendKeys("2");
-            input2.SendKeys("2");
-            sumButton.Click();
+        }
 
-            IWebElement result = chrome.FindElement(By.Id("displayvalue"));
-            //result = result.GetAttribute("value");
-            Assert.AreEqual("4", result.Text, "Result is wrong");
+        [OneTimeTearDown] 
+        public static void OneTimeTearDown()
+        {
+            chrome.Quit();
         }
         
-        [Test]
-        public static void TestTwoInputFields2()
+        [TestCase("2", "2", "4", TestName = "2plus2")]
+        [TestCase("-5", "3", "-2", TestName = "-5plus3")]
+        [TestCase("a", "b", "NaN", TestName = "aplusb")]
+        public static void TestCases(string num1, string num2, string resultTxt)
         {
-            IWebDriver chrome = new ChromeDriver(); //sukuriamas objektas, jo pavadinimas chrome
-            chrome.Url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
-            //uzdaryti pop-up
-            WebDriverWait wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(10));
-            wait.Until(d =>
-                d.FindElement(By.CssSelector("#at-cv-lightbox-close"))
-                    .Displayed); //d kaip atitikmuo driver, galima irasyti ka nori
-            chrome.FindElement(By.CssSelector("#at-cv-lightbox-close")).Click();
-
             IWebElement input1 = chrome.FindElement(By.Id("sum1"));
             IWebElement input2 = chrome.FindElement(By.Id("sum2"));
             IWebElement sumButton = chrome.FindElement(By.CssSelector("#gettotal > button"));
             
-            input1.SendKeys("-5");
-            input2.SendKeys("3");
+            input1.Clear();
+            input1.SendKeys(num1);
+            input2.Clear();
+            input2.SendKeys(num2);
             sumButton.Click();
 
             IWebElement result = chrome.FindElement(By.Id("displayvalue"));
-            //result = result.GetAttribute("value");
-            Assert.AreEqual("-2", result.Text, "Result is wrong");
-        }
-        
-        [Test]
-        public static void TestTwoInputFields3()
-        {
-            IWebDriver chrome = new ChromeDriver(); //sukuriamas objektas, jo pavadinimas chrome
-            chrome.Url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
-            //uzdaryti pop-up
-            WebDriverWait wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(10));
-            wait.Until(d =>
-                d.FindElement(By.CssSelector("#at-cv-lightbox-close"))
-                    .Displayed); //d kaip atitikmuo driver, galima irasyti ka nori
-            chrome.FindElement(By.CssSelector("#at-cv-lightbox-close")).Click();
-
-            IWebElement input1 = chrome.FindElement(By.Id("sum1"));
-            IWebElement input2 = chrome.FindElement(By.Id("sum2"));
-            IWebElement sumButton = chrome.FindElement(By.CssSelector("#gettotal > button"));
-            
-            input1.SendKeys("a");
-            input2.SendKeys("b");
-            sumButton.Click();
-
-            IWebElement result = chrome.FindElement(By.Id("displayvalue"));
-            //result = result.GetAttribute("value");
-            Assert.AreEqual("NaN", result.Text, "Result is wrong");
-            
+            Assert.AreEqual(resultTxt, result.Text, "Result is wrong");
         }
     }
 }
